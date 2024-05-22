83a0497f5be4b21320ae468492657235a6d29d3e
Lawrence <mstrlaw@users.noreply.github.com>
2024-05-22 17:40:05 +0100

    Test: Merge branch into Main (#4)
    Adds first working version of CI/CD workflow for deploying preview link when pushing code.
    Adds mocked workflows for PR creation and tag creation.

--- tests/index.spec.js
+++
@@ -1,9 +0,0 @@
-// @ts-check
-const { test, expect } = require('@playwright/test');
-
-test('Hello world visible', async ({ page }) => {
-  await page.goto('http://127.0.0.1:8080/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page.getByText('Hello world')).toBeVisible()
-});
