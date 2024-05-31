8d66754cb39968116b62ec00b3a2019d532ddaac
Lawrence <lawbraun.almeida@gmail.com>
2024-05-31 12:02:39 +0100

    Adds the new feedback from someone

--- tests/homepage.spec.js
+++ tests/homepage.spec.js
@@ -3,10 +3,10 @@ const { test, expect } = require('@playwright/test');
 
 const START_PAGE = 'http://127.0.0.1:8080/'
 
-test('Homepage displays Hello Bananas', async ({ page }) => {
+test('Homepage displays Hello World', async ({ page }) => {
   await page.goto(START_PAGE);
 
-  await expect(page.getByText('Hello Bananas')).toBeVisible();
+  await expect(page.getByText('Hello World')).toBeVisible();
 });
 
 test('Homepage has a navigation link', async ({ page }) => {
